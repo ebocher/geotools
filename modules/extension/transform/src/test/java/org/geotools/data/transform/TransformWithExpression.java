a6eb469d11d7824a7e34fd87c18c04c0f8c3b36b
ebocher <erwan.bocher@univ-ubs.fr>
2021-03-11 18:15:12 +0100

    Prepare ifthenelse detect super datatype

---
+++ modules/extension/transform/src/test/java/org/geotools/data/transform/TransformWithExpression.java
@@ -0,0 +1,100 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2002-2021, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.data.transform;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.geotools.data.DataUtilities;
+import org.geotools.data.memory.MemoryDataStore;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.feature.SchemaException;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.filter.FilterFactoryImpl;
+import org.geotools.filter.IllegalFilterException;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.Polygon;
+import org.locationtech.jts.io.ParseException;
+import org.locationtech.jts.io.WKTReader;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.filter.expression.Function;
+
+/** @author Erwan Bocher, CNRS 2021 */
+public class TransformWithExpression {
+
+    static FilterFactoryImpl ff;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        ff = new FilterFactoryImpl();
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        ff = null;
+    }
+
+    @Test
+    public void testIThenElse4()
+            throws IllegalFilterException, SchemaException, ParseException, IOException {
+        MemoryDataStore memory = new MemoryDataStore();
+        SimpleFeatureType type =
+                DataUtilities.createType(
+                        "testSchema", "name:String,gid:Integer,*the_geom:Geometry");
+        WKTReader reader = new WKTReader();
+        Geometry geom1 = reader.read("LINESTRING(0 0 0, 10 10 10)");
+        SimpleFeature feature1 =
+                SimpleFeatureBuilder.build(type, new Object[] {"testFeature1", 1, geom1}, null);
+        Geometry geom2 = reader.read("LINESTRING(0 0 0, 10 10 10)");
+        SimpleFeature feature2 =
+                SimpleFeatureBuilder.build(type, new Object[] {"testFeature2", 2, geom2}, null);
+        ArrayList<SimpleFeature> dataFeatures = new ArrayList<>();
+        memory.addFeature(feature1);
+        memory.addFeature(feature2);
+        List<Definition> definitions = new ArrayList<Definition>();
+        Function if_ = ff.function("equalto", ff.property("gid"), ff.literal(2));
+        Function then = ff.function("buffer", ff.property("the_geom"), ff.literal(20));
+        Function if_then_else = ff.function("if_then_else", if_, then, ff.property("the_geom"));
+        definitions.add(new Definition("THE_GEOM", if_then_else));
+        SimpleFeatureSource transformed =
+                TransformFactory.transform(
+                        memory.getFeatureSource("testSchema"), "OUTPUT_TABLE_TEST_F", definitions);
+        SimpleFeatureCollection simpleFeatureCollection = transformed.getFeatures();
+        SimpleFeatureIterator features = simpleFeatureCollection.features();
+        try {
+            int nbPoint = 0;
+            while (features.hasNext()) {
+                SimpleFeature feature = features.next();
+                Geometry geom = (Geometry) feature.getDefaultGeometry();
+                if (geom instanceof Polygon) {
+                    nbPoint++;
+                }
+            }
+            assertEquals(1, nbPoint);
+        } finally {
+            features.close();
+        }
+    }
+}
---
+++ modules/library/main/src/test/java/org/geotools/filter/IfThenElseFunctionTest.java
@@ -0,0 +1,73 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2002-2021, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.filter;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.locationtech.jts.io.ParseException;
+import org.opengis.filter.expression.Function;
+
+/**
+ * @author Erwan Bocher, CNRS 2021
+ */
+public class IfThenElseFunctionTest {
+
+    static FilterFactoryImpl ff;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        ff = new FilterFactoryImpl();
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        ff = null;
+    }
+
+    @Test
+    public void testIThenElse1() throws IllegalFilterException {
+        Function if_ = ff.function("equalto", ff.literal("1"), ff.literal(2));
+        Function if_then_else = ff.function("if_then_else", if_, ff.literal(1), ff.literal(-1));
+        Object result = if_then_else.evaluate(new Object());
+        Assert.assertEquals(-1, result);
+    }
+
+    @Test
+    public void testIThenElse2() throws IllegalFilterException {
+        Function if_ = ff.function("equalto", ff.literal("1"), ff.literal(2));
+        Function if_then_else = ff.function("if_then_else", if_, ff.literal(1), ff.literal(-1.55));
+        Object result = if_then_else.evaluate(new Object());
+        Assert.assertEquals(-1.55, result);
+    }
+
+    @Test
+    public void testIThenElse3() throws IllegalFilterException, ParseException {
+        Function if_ = ff.function("equalto", ff.literal("1"), ff.literal(2));
+        Function geom
+                = ff.function(
+                        "geomFromWKT",
+                        ff.literal("POINT (0 0)"));
+        Function else_ = ff.function("toWKT", ff.function("buffer", geom, ff.literal(20)));
+        Function if_then_else = ff.function("if_then_else", if_, ff.literal(1), else_);
+        Object result = if_then_else.evaluate(new Object());
+        Assert.assertEquals("POLYGON ((20 0, 19.61570560806461 -3.9018064403225647, 18.477590650225736 -7.653668647301796, 16.629392246050905 -11.111404660392044, 14.142135623730951 -14.14213562373095, 11.111404660392045 -16.629392246050905, 7.653668647301797 -18.477590650225736, 3.9018064403225665 -19.61570560806461, 0.0000000000000012 -20, -3.901806440322564 -19.61570560806461, -7.653668647301794 -18.477590650225736, -11.11140466039204 -16.629392246050905, -14.14213562373095 -14.142135623730951, -16.629392246050905 -11.111404660392044, -18.477590650225736 -7.653668647301798, -19.61570560806461 -3.9018064403225723, -20 -0.0000000000000024, -19.61570560806461 3.9018064403225674, -18.477590650225736 7.653668647301793, -16.62939224605091 11.11140466039204, -14.142135623730955 14.14213562373095, -11.111404660392044 16.629392246050905, -7.6536686473018065 18.47759065022573, -3.901806440322573 19.615705608064605, -0.0000000000000037 20, 3.901806440322566 19.61570560806461, 7.6536686473018 18.477590650225732, 11.111404660392036 16.62939224605091, 14.142135623730947 14.142135623730955, 16.629392246050905 11.111404660392044, 18.47759065022573 7.653668647301808, 19.615705608064605 3.9018064403225745, 20 0))", result);
+    }
+
+   
+}
